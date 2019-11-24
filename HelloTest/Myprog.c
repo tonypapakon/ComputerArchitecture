209fc1dc7a863c3e9022f12a6f14be6dbeea473d
tonypapakon <57982810+tonypapakon@users.noreply.github.com>
2019-11-24 19:09:05 +0200

    Create Myprog.c

---
+++ HelloTest/Myprog.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main()
+{
+
+     //random generator//
+     srand(time(NULL));   
+     int r = rand();
+       
+     printf("My random number is %d:/",r);
+     return 0;
+}
